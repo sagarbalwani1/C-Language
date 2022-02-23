4d9f12d99ff8aa25ad521c6d79f8bf669ed9287a
Sagar Balwani <sagarbalwani1@gmail.com>
2022-02-22 22:48:49 -0500

    Create flag01.c

---
+++ patterns/flag01.c
@@ -0,0 +1,37 @@
+// flag01.c - Make an Indian Flag without colors. Since we haven't been taught that yet.😅️
+// 
+# include <stdio.h>
+void main(){
+	printf("  **  \n");
+	printf(" *  ****************************************** \n");
+	printf(" *  **                                       * \n");
+	printf(" *  **                                       * \n");
+	printf(" *  ****************************************** \n");
+	printf(" *  **                 🌐️🌐️                  * \n");
+	printf(" *  **                 🌐️🌐️                  * \n");
+	printf(" *  ****************************************** \n");
+	printf(" *  **                                       * \n");
+	printf(" *  **                                       * \n");
+	printf(" *  ****************************************** \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf(" *  * \n");
+	printf("  **  \n");
+}
